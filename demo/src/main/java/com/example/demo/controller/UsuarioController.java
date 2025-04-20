f1aa611103e121756d27b5723168ee4719b62326
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-04-19 21:17:39 -0500

    Creacion del DTO de usuario y de la clase usuario

--- demo/src/main/java/com/example/demo/controller/UsuarioController.java
+++ demo/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UsuarioDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UsuarioController {
+
 }
+
--- demo/src/main/java/com/example/demo/dto/UsuarioDTO.java
+++ demo/src/main/java/com/example/demo/dto/UsuarioDTO.java
@@ -1,18 +1,18 @@
-package com.example.demo.record;
+package com.example.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
-public record Usuario(
+public record UsuarioDTO(
         @NotBlank(message = "El campo es requerido")
         @Email(message = "Debe ser un email válido")
-        String cooreo,
+        String correo,
         @NotBlank(message = "El campo es requerido")
         @Pattern(regexp = "^(?=.*\\\\d)(?=.*[a-z])(?=.*[A-Z]).*$",message = "Debe contener a")
         @Size(min = 8,message = "La longitud mínima es 8")
-        String contraseña,
+        String contrasenia,
         @NotBlank(message = "El campo es requerido")
         @Size(max = 100,message = "No debe exceder los 100 caracteres")
         String nombreCompleto,
