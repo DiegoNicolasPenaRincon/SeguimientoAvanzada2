d6981bebb22faba5597930608b45d4cf36a7ae14
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-04-19 20:48:47 -0500

    Se crearon cosistas

---
+++ demo/src/main/java/com/example/demo/record/Usuario.java
@@ -0,0 +1,23 @@
+package com.example.demo.record;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+public record Usuario(
+        @NotBlank(message = "El campo es requerido")
+        @Email(message = "Debe ser un email válido")
+        String cooreo,
+        @NotBlank(message = "El campo es requerido")
+        @Pattern(regexp = "^(?=.*\\\\d)(?=.*[a-z])(?=.*[A-Z]).*$",message = "Debe contener a")
+        @Size(min = 8,message = "La longitud mínima es 8")
+        String contraseña,
+        @NotBlank(message = "El campo es requerido")
+        @Size(max = 100,message = "No debe exceder los 100 caracteres")
+        String nombreCompleto,
+        @NotNull(message = "La fecha no puede ser nula")
+        @PastOrPresent(message = "La fecha no puede ser futura")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        LocalDate fechaNacimiento) {
+}
